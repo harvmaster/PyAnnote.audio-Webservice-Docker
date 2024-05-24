c63167c8dd6bb3dad6b1b1a5048b753d7fd30778
Harvmaster <hzuccon@gmail.com>
2024-05-24 19:25:20 +1000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use the official Python image as the base image
+FROM python:3.8
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the Python scripts and requirements file
+COPY app.py main.py requirements.txt ./
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port that the FastAPI app will run on
+EXPOSE 3000
+
+# Command to run the FastAPI app
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000"]
---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from pyannote.core import Segment, Annotation
+from pyannote.audio import Pipeline
+
+HF_API_KEY = os.environ.get("HF_API_KEY")
+ORIGINS = os.environ.get("ALLOW_ORIGINS")
+
+pipeline = Pipeline.from_pretrained(
+  "pyannote/speaker-diarization-3.1",
+  use_auth_token=HF_API_KEY)
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+class SpeakerInfo(BaseModel):
+    speaker: str
+    start: float
+    end: float
+
+@app.post("/get_speaker_data/")
+async def get_speaker_data(audio_file: UploadFile = File(...)):
+    audio_data = audio_file.file
+
+    diarization = pipeline({"uri": audio_file.filename, "audio": audio_data})
+    annotation = diarization.get_timeline().to_annotation()
+    speaker_data = []
+
+    for segment in annotation.itersegments():
+        speaker_data.append(SpeakerInfo(speaker=segment[2], start=segment[0], end=segment[1]))
+
+    return speaker_data
+
